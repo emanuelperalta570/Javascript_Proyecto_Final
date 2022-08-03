c35ccfc50917a22a08955e52d066a78014f98da9
Emanuel <emanuel.peralta@live.com.ar>
2022-08-03 10:14:56 -0300

    Emanuel Peralta Proyecto Final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+
 let nombre;
 let sexo;
 let dni;
@@ -25,8 +26,23 @@ fetch("../js/data.json")
         lista.append(li);
   });
 });
-
-
+//AGREGADO DE FUNCION QUE RECIBE VALOR
+function validarDNI(x){
+    if(x.toString().length<9 && x.toString().length>6 ){
+        swal({
+            title: "Genial",
+            text: "El DNI ingresado es correcto",
+            icon:"success",
+        })
+        return 1;
+    }else{
+        swal({
+            title: "El DNI ingresado es incorrecto",
+            icon:"warning"
+        })
+        return -1;
+    }
+}
 
 class Cliente{
     constructor(nombreUsuario,dniUsuario,cantEmpleados,mail){
@@ -47,14 +63,17 @@ class Cliente{
 }
 
 class ServicioSolic{
-    constructor(nom_usuario,q_emp,valorTotal){
+    constructor(nom_usuario,q_emp,valorTotal,mail){
         this.nom_usuario=nom_usuario;
         this.q_emp=q_emp;
         this.valorTotal=valorTotal;
+        this.mail=mail;
     }
 }
 
 let indexUsuario=-2;
+let indexDNI=-2;
+let indexQEMP=-2;
 const clientes=[];
 
 const cliente1 = new Cliente ("EMANUEL",36873055,5,"ema@gmail.com");
@@ -73,6 +92,8 @@ for(const cliente of clientes){
     cliente.calcularTotal();
 }
 
+
+//VALIDACION DE DATOS
 do{
     do {
         if(indexUsuario==-2){
@@ -87,6 +108,9 @@ do{
                             text: "Nombre de usuario en uso,ingrese otro nombre de usuario",
                             icon:"warning",
                         })
+
+                        var nombre = document.getElementById("nombre");
+                        nombre.value=nombre.defaultValue;
                         break;
                     }
                     else{
@@ -96,34 +120,32 @@ do{
             if(indexUsuario<0){
                 let titulo = document.getElementById("titulo")
                 titulo.innerText=`Bienvenido ${nombre} !`
+                swal({
+                    title: "Genial",
+                    text: "El usuario ingresado esta disponible",
+                    icon:"success",
+                })
             }
-            }  
-    }   else{
-            nombre = document.getElementById("nombre");
-            nombre.onchange = (e) => {
-            nombre=e.target.value.toUpperCase();
-                for(const cliente of clientes){
-                    if(nombre==cliente.nombre){
-                        indexUsuario=1;
-                        swal({
-                            title: "Ups",
-                            text: "Nombre de usuario en uso,ingrese otro nombre de usuario",
-                            icon:"warning",
-                        })
-                        break;
-                    }
-                    else{
-                        indexUsuario=-1;
-                    }
-                }
-            if(indexUsuario<=0){
-                    let titulo = document.getElementById("titulo")
-                    titulo.innerText=`Bienvenido ${nombre} !`
-                }     
             }  
         }
     }while(indexUsuario>=0)
-} while((nombre == "") || (dni==NaN ))
+
+        if(indexDNI==-2){
+            dni=document.getElementById("dni");
+            dni.onchange = (e) => {
+            dni=e.target.value;
+            indexDNI=validarDNI(dni);
+                if(indexDNI==-1){
+                    var dni = document.getElementById("dni");
+                    dni.value = dni.defaultValue;
+                }
+            }
+        }
+
+
+
+    
+} while((nombre == "") || (dni==NaN) || cantEmpleados=="")
 
 
 
@@ -144,35 +166,28 @@ mail.onchange = (e) =>{
 let enviar = document.getElementById("btn-enviar");
 enviar.onclick= (e) => {
     e.preventDefault();
-    let cliente6 = new Cliente (nombre,dni,cantEmpleados,mail);
+
+    let nombre = document.getElementById("nombre");
+
+    //VALIDACION DE DATOS
+    if(nombre.value=="" || dni.value==""||  mail.value==""|| cantEmpleados.value==""){
+        swal({
+            title: "Ups",
+            text: "Falta ingresar datos",
+            icon:"warning",
+        })
+    }else{
+
+    let cliente6 = new Cliente (nombre.value.toUpperCase(),dni.value,cantEmpleados,mail);
     clientes.push(cliente6);
     cliente6.calcularTotal();
     let container = document.getElementById("contenedor-body");
     cliente6.mail="" ? cliente6.mail="NULL": cliente6.mail=cliente6.mail ;
-
-
-    swal({
-        title: "Genial",
-        text: "La carga de datos ha sido exitosa",
-        icon:"success",
-    })
-
-    cantEmpleados!=0 ? container.innerHTML=`<h3>DETALLE DE SERVICIO</h3> 
-                        <p>Servicio solicitado para ${cantEmpleados} empleados</p> 
-                        <p>El valor total del servicio es de $${cliente6.total} pesos </p>
-                        <p>Su mail de contacto es: ${cliente6.mail}</p>
-                        <p>Gracias por confiar en nosotros, estaremos en contacto a la brevedad</p>`
-                     :
-    container.innerHTML=`<h3>DETALLE DE SERVICIO</h3> 
-        <p>Servicio solicitado sin empleados</p> 
-        <p>El valor total del servicio es de $${cliente6.total} pesos </p>
-        <p>Su mail de contacto es: ${cliente6.mail}</p>
-        <p>Gracias por confiar en nosotros, estaremos en contacto a la brevedad</p>`
-
-
     
-    let servicioEnCarrito = new ServicioSolic (cliente6.nombre,cantEmpleados,cliente6.total);
+   
+    let servicioEnCarrito = new ServicioSolic (cliente6.nombre,cantEmpleados,cliente6.total,cliente6.mail);
 
+    //USO DEL LOCAL STORAGE
     const servJson = JSON.stringify(servicioEnCarrito);
        
     localStorage.setItem('ServicioSolicitado',servJson);
@@ -193,6 +208,32 @@ enviar.onclick= (e) => {
     for (const usuario of usuariosTotales){
             cantidadClientes++;
     }
+
+    let objServ = JSON.parse(localStorage.getItem('ServicioSolicitado'));
+
+    
+    
+    cantEmpleados!=0 ? container.innerHTML=`<h3>DETALLE DE SERVICIO</h3> 
+    <p>Servicio solicitado a nombre de ${objServ.nom_usuario}</p> 
+    <p>Para ${objServ.q_emp} empleados</p> 
+    <p>El valor total del servicio es de $${objServ.valorTotal} pesos </p>
+    <p>Su mail de contacto es: ${objServ.mail}</p>
+    <p>Gracias por confiar en nosotros, estaremos en contacto a la brevedad</p>`
+    :
+
+    container.innerHTML=`<h3>DETALLE DE SERVICIO</h3> 
+    <p>Servicio solicitado a nombre de ${objServ.nom_usuario} empleados</p> 
+    <p>Servicio solicitado sin empleados</p> 
+    <p>El valor total del servicio es de $${objServ.valorTotal} pesos </p>
+    <p>Su mail de contacto es: ${objServ.mail}</p>
+    <p>Gracias por confiar en nosotros, estaremos en contacto a la brevedad</p>`
+
+    swal({
+        title: "Genial",
+        text: "La carga de datos ha sido exitosa",
+        icon:"success",
+    })
+}
 }
 
 
